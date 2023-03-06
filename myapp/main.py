bf7134095574f3d88419170bf16cfe899987ea42
Green0x0y <66325429+Green0x0y@users.noreply.github.com>
2023-03-06 19:38:20 +0100

    LoginScreen finished

--- myapp/main.py
+++ myapp/main.py
@@ -8,6 +8,7 @@ Builder.load_file('mainscreen.kv')
 
 class LoginScreen(Screen):
     def login(self, instance):
+
         # Check if the username and password are correct
         if self.ids.username_input.text == 'admin' and self.ids.password_input.text == 'admin123':
             # Switch to the main screen
@@ -16,21 +17,37 @@ class LoginScreen(Screen):
             self.ids.password_input.text = ''
 
 
+
 class MainScreen(Screen):
     def logout(self, instance):
-        # Switch back to the login screen
+    # Switch back to the login screen
         self.manager.current = 'login'
+
+class UserCodeScreen(Screen):
+    def verify_code(self, instance):
+        code = self.ids.code_input.text
+        if len(code) != 8:
+            print("Code must be 8 characters long")
+        elif not code.isalnum():
+            print("Code must contain only letters and numbers")
+        else:
+        # TODO: Handle valid code
+            print("Valid code entered")
 
 
 class MyApp(App):
+    username =''
     def build(self):
         # Create the screen manager and add the login and main screens to it
         screen_manager = ScreenManager()
         screen_manager.add_widget(LoginScreen(name='login'))
         screen_manager.add_widget(MainScreen(name='main'))
+        screen_manager.add_widget(UserCodeScreen(name='user_code'))
 
         return screen_manager
 
 
 if __name__ == '__main__':
     MyApp().run()
+
+
