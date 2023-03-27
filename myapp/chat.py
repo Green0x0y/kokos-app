59ce6b86e799456ba09eceba5c257e09815dde98
socz3qqq <soczawa@student.agh.edu.pl>
2023-03-27 03:53:27 +0200

    Added chats and settings screen, improved screen manager

---
+++ myapp/chat.py
@@ -0,0 +1,126 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.scrollview import ScrollView
+from kivy.graphics import Color, Rectangle, RoundedRectangle
+from kivy.uix.tabbedpanel import TabbedPanelItem
+from kivy.core.window import Window
+from kivy.lang import Builder
+
+Builder.load_file('custom_widgets/roundedinput.kv')
+Builder.load_file('custom_widgets/roundedbutton.kv')
+
+class ChatWindow(TabbedPanelItem):
+    def __init__(self, user, user_data, **kwargs):
+        super(ChatWindow, self).__init__(**kwargs)
+        self.text=user
+
+        layout = BoxLayout(orientation='vertical',
+                            size_hint=(1, 1))
+        chat_box = ChatBox(self, user, user_data[user])
+
+        send_message = SendMessage()
+
+        layout.add_widget(chat_box)
+        layout.add_widget(send_message)
+        self.add_widget(layout)
+
+
+class ChatBox(ScrollView):
+    def __init__(self, parent, user, user_messages, **kwargs):
+        super(ChatBox, self).__init__(**kwargs)
+        self.size_hint=(1, 0.9)
+        self.margin=10
+
+        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+        layout.bind(minimum_height=layout.setter('height'))
+        for msg in user_messages:
+            new_message = Message(parent, user + " on " + msg["datetime"] + " wrote: ", msg["message"])
+            layout.add_widget(new_message)
+
+        self.add_widget(layout)
+        with self.canvas.before:
+            Color(0.4, 0.6, 1, 1) # blue color
+            self.rect = Rectangle(pos=self.pos, size=self.size)
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
+
+class Message(BoxLayout):
+    def __init__(self, parent, text, msg, **kwargs):
+        super(Message, self).__init__(**kwargs)
+
+        self.orientation='vertical'
+        self.spacing = 5
+        self.padding = [10, 10]
+        self.size_hint_y = None
+        self.add_widget(MessageLabel(text))
+        label = MessageContent(parent, msg)
+
+        self.add_widget(label)
+
+class MessageContent(Label):
+    def __init__(self, parent, text, **kwargs):
+        super(MessageContent, self).__init__(**kwargs)
+        self.text = text
+        self.size_hint_x= None
+        self.color = 0, 0, 0, 1
+        self.font_size = parent.height/5
+        self.font_name = 'Roboto-Bold.ttf'
+        self.bold=True
+        self.padding = (30, 100)
+        self.texture_update()
+        self.width = self.texture_size[0]
+
+        with self.canvas.before:
+            Color(1, 1, 1, 1) # white
+            self.rect = RoundedRectangle(pos=self.pos, size=self.size, radius=[20])
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
+
+class MessageLabel(Label):
+    def __init__(self, text, **kwargs):
+        super(MessageLabel, self).__init__(**kwargs)
+        self.text = text
+        self.size_hint_x= None
+        self.color = 0.2, 0.2, 0.2, 0.7
+        self.font_name = 'Roboto-Bold.ttf'
+        self.texture_update()
+        self.width = self.texture_size[0]
+
+class SendMessage(BoxLayout):
+    def __init__(self, **kwargs):
+        super(SendMessage, self).__init__(**kwargs)
+
+        self.orientation='horizontal'
+        self.size_hint=(1, 0.1)
+        self.spacing= 30
+        self.padding = 10
+
+        with self.canvas.before:
+            Color(0.4, 0.6, 1, 1)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+
+        self.add_widget(RoundedInput())
+        self.add_widget(RoundedButton("send"))
+
+    def on_size(self, *args):
+        self.rect.size = self.size
+        self.rect.pos = self.pos
+
+class RoundedButton(Button):
+    def __init__(self, text, **kwargs):
+        super(RoundedButton, self).__init__(**kwargs)
+
+        self.size_hint = (0.2, 1)
+        self.text = text
+
+class RoundedInput(TextInput):
+    pass
--- myapp/main.py
+++ myapp/main.py
@@ -2,14 +2,19 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-from  kivy_garden.zbarcam import ZBarCam
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.graphics import Color, Rectangle
+from chat import ChatWindow
+import json
 
-Builder.load_file('loginscreen.kv')
-Builder.load_file('mainscreen.kv')
-Builder.load_file('usercodescreen.kv')
-Builder.load_file('registrationscreen.kv')
-Builder.load_file('signupscreen.kv')
-Builder.load_file('qrscreen.kv')
+Builder.load_file('screens/loginscreen.kv')
+Builder.load_file('screens/mainscreen.kv')
+Builder.load_file('screens/usercodescreen.kv')
+Builder.load_file('screens/registrationscreen.kv')
+Builder.load_file('screens/signupscreen.kv')
+Builder.load_file('screens/qrscreen.kv')
+Builder.load_file('screens/settingsscreen.kv')
+# Builder.load_file('chatsscreen.kv')
 Window.size = (1000, 1000)
 
 class LoginScreen(Screen):
@@ -31,13 +36,24 @@ class MainScreen(Screen):
 
     def switch_to_user_code_screen(self, instance):
         # Switch to the user code screen
+        self.manager.transition.direction = "left"
         self.manager.current = 'user_code'
     def switch_to_registration_screen(self, instance):
         # Switch to the user code screen
+        self.manager.transition.direction = "left"
         self.manager.current = 'registration'
     def switch_to_qr_screen(self, instance):
         # Switch to the user code screen
+        self.manager.transition.direction = "left"
         self.manager.current = 'qr'
+    def switch_to_settings_screen(self, instance):
+        # Switch to settings screen
+        self.manager.transition.direction = "left"
+        self.manager.current = 'settings'
+    def switch_to_chats_screen(self, instance):
+        # Switch to chats screen
+        self.manager.transition.direction = "left"
+        self.manager.current = 'chats'
 
 
 class UserCodeScreen(Screen):
@@ -64,8 +80,32 @@ class QRScreen(Screen):
     def show_qr_code(self, instance, symbol):
         self.ids.qr_code_label.text = f"QR code found"
 
-class MyApp(App):
+class ChatsScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        file = open("myapp/data/mock_data.json", "r")
+        data = json.load(file)
+        file.close()
+        
+        chatsPanel = TabbedPanel(do_default_tab=False,
+                                 tab_pos='left_top')
+        for user in data['users']['1']['conversation_to']:
+            chat = ChatWindow(user, data['users']['1']['conversation_to'])
+            chatsPanel.add_widget(chat)
 
+        self.add_widget(chatsPanel)
+
+
+class SettingsScreen(Screen):
+    def switch_click(self, switchObject, switchValue):
+        if(switchValue):
+            self.ids.mail_label.text = "Powiadomienia mailowe włączone"
+        else:
+            self.ids.mail_label.text = "Powiadomienia mailowe wyłączone"
+
+
+class MyApp(App):
+    users = ["user1", "user2", "user3"]
     def build(self):
 
         Window.bind(on_keyboard=self.on_key)
@@ -77,6 +117,8 @@ class MyApp(App):
         screen_manager.add_widget(RegistrationScreen(name='registration'))
         screen_manager.add_widget(UserCodeScreen(name='user_code'))
         screen_manager.add_widget(SignUpScreen(name='signup'))
+        screen_manager.add_widget(ChatsScreen(name='chats'))
+        screen_manager.add_widget(SettingsScreen(name='settings'))
 
         return screen_manager
 
@@ -86,9 +128,23 @@ class MyApp(App):
         if key == 27:  # 27 is the keycode for the back button
             # Switch to the previous screen
             if self.root.current == 'user_code':
+                self.root.transition.direction = "right"
                 self.root.current = 'main'
                 return True
             elif self.root.current == 'registration':
+                self.root.transition.direction = "right"
+                self.root.current = 'login'
+                return True
+            elif self.root.current == 'chats':
+                self.root.transition.direction = "right"
+                self.root.current = 'main'
+                return True
+            elif self.root.current == 'settings':
+                self.root.transition.direction = "right"
+                self.root.current = 'main'
+                return True
+            elif self.root.current == 'qr':
+                self.root.transition.direction = "right"
                 self.root.current = 'main'
                 return True
 
